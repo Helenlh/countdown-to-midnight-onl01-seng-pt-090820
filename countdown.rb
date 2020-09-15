7c988f5fe4658819b92c5b545227d232b0d33958
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-15 21:26:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,9 @@
 #write your code here
 
-def countdown(num)
-  while num>0 
+def countdown(number)
+  while number > 0 
     puts "#{number} SECOND(S)!"
+    number +=1 
 end
 return "HAPPY NEW YEAR!"
 end 
